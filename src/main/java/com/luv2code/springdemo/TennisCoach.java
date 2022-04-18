1de4545137069d20f4219156ca2f084d8a776cc9
SandraRusu <sandra.rusu.ext@extendaretail.com>
2022-04-18 15:38:51 +0300

    Defining init and destroy methods for TennisCoach class

--- src/main/java/com/luv2code/springdemo/TennisCoach.java
+++ src/main/java/com/luv2code/springdemo/TennisCoach.java
@@ -1,6 +1,6 @@
 package com.luv2code.springdemo;
 
-public class TennisCoach implements Coach{
+public class TennisCoach implements Coach {
     @Override
     public String getDailyWorkout() {
         return "Spent 1 hour for training";
@@ -10,4 +10,13 @@ public class TennisCoach implements Coach{
     public String getDailyFortune() {
         return null;
     }
+
+    public void doMyStartupStuff() {
+        System.out.println("TennisCoach: inside the method doMyStartupStuff");
+    }
+
+    public void doMyCleanupStuff() {
+        System.out.println("TennisCoach: inside the method doMyCleanupStuff");
+    }
+
 }
